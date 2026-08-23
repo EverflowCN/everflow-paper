1ec7df9e24c1c71c1f215581088e65fb6ada94a1
Everflow <54564601+EverflowCN@users.noreply.github.com>
2026-08-23 11:35:36 +0800

    Load SRS reset controls after stats

--- site/assets/js/site-runtime-v2.js
+++ site/assets/js/site-runtime-v2.js
@@ -16,6 +16,7 @@
     import('/assets/js/zhenti-qwer.js?v=20260823g').catch(err=>console.error('Everflow 408 QWER shortcuts failed',err));
     import('/assets/js/zhenti-srs.js?v=20260823a')
       .then(()=>import('/assets/js/zhenti-srs-error.js?v=20260823a'))
+      .then(()=>import('/assets/js/zhenti-srs-reset.js?v=20260823a'))
       .catch(err=>console.error('Everflow 408 SRS quick cards failed',err));
   }
 })();
